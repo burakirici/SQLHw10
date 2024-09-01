6e476c102bb0c461265a32de8b78c9028bb00a73
Burak Kırıcı <burakpinar76@gmail.com>
2024-09-01 21:57:15 +0300

    Add files via upload

---
+++ Hw9.sql
@@ -0,0 +1,16 @@
+-- Question 1
+--SELECT city.city, country.country
+--FROM city
+--LEFT JOIN country ON city.country_id = country.country_id;
+
+-- Question 2
+
+--SELECT payment.payment_id, customer.first_name, customer.last_name
+--FROM payment
+--RIGHT JOIN customer ON payment.customer_id = customer.customer_id;
+
+-- Question 3
+
+--SELECT rental.rental_id, customer.first_name, customer.last_name
+--FROM rental
+--FULL JOIN customer ON rental.customer_id = customer.customer_id;
